3b79f7d88b36d76f77d41de3047eb9947e8bd718
Kinzie F <kinziefriesen47@gmail.com>
2023-09-26 12:11:56 -0500

    Added name to String

--- GithubTest/src/githubTest/GithubTest.java
+++ GithubTest/src/githubTest/GithubTest.java
@@ -3,7 +3,7 @@ package githubTest;
 public class GithubTest {
 
 	public static void main(String[] args) {
-		String[] names = {"Kinzie", "Vlad", "Roain"};
+		String[] names = {"Kinzie", "Vlad", "Roain", "Mr Neufeld"};
 		
 		for(int i = 0; i < names.length; i++) {
 			System.out.println(names[i]);
