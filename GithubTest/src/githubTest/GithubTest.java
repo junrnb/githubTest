4bcec45ad08b0678f36610447f64c1730899da8f
Kinzie F <kinziefriesen47@gmail.com>
2023-09-26 11:46:39 -0500

    Committed and pushed GithubTest

---
+++ GithubTest/src/githubTest/GithubTest.java
@@ -0,0 +1,13 @@
+package githubTest;
+
+public class GithubTest {
+
+	public static void main(String[] args) {
+		String[] names = {"Kinzie", "Vlad", "Roain"};
+		
+		for(int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+	}
+
+}
