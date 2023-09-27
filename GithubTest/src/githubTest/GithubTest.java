113f6f230ce0e3d5df98903f3f04a378bfad5af1
Kinzie F <kinziefriesen47@gmail.com>
2023-09-27 12:02:36 -0500

    Added text to print line

--- GithubTest/src/githubTest/GithubTest.java
+++ GithubTest/src/githubTest/GithubTest.java
@@ -5,7 +5,7 @@ public class GithubTest {
 	public static void main(String[] args) {
 		String[] names = {"Kinzie", "Vlad", "Roain", "Mr Neufeld","Text"};
 		
-		System.out.println("Computer Science 40S: ");
+		System.out.println("Computer Science 40S 2023-2024: ");
 		
 		for(int i = 0; i < names.length; i++) {
 			System.out.println(names[i]);
