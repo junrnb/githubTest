cff2898fbdb8c21dcce4256d70e88b7f26a60460
Kinzie F <kinziefriesen47@gmail.com>
2023-09-27 11:13:54 -0500

    Added line before names are printed out

--- GithubTest/src/githubTest/GithubTest.java
+++ GithubTest/src/githubTest/GithubTest.java
@@ -5,6 +5,8 @@ public class GithubTest {
 	public static void main(String[] args) {
 		String[] names = {"Kinzie", "Vlad", "Roain", "Mr Neufeld"};
 		
+		System.out.println("Computer Science 40S: ");
+		
 		for(int i = 0; i < names.length; i++) {
 			System.out.println(names[i]);
 		}
